eef2a661edd444184bb699d0a39b04b58bf3b5c2
Starlet55 <godwinuche2@gmail.com>
2018-03-16 16:56:36 +0100

    still on server.js

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
+const express = require('express')
+const app = express()
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
