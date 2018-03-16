a39c93afb91b40d4ffdd16fb0b48602a3bc4d552
Starlet55 <godwinuche2@gmail.com>
2018-03-16 14:06:39 +0100

    confused with this application error stuff

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-const express = require('express')
-const app = express()
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
