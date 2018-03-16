441f968eb9ed63fa114acab1adda052758accf7f
Starlet55 <godwinuche2@gmail.com>
2018-03-16 17:16:44 +0100

    server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+})
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000'));
